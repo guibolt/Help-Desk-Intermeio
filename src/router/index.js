a98f0247892fe9894df25d68e60184d04e114a2d
DrFaelSan <rafaelvplima@gmail.com>
2020-01-06 11:02:02 -0300

    atualização part 1 06-01

--- src/router/index.js
+++ src/router/index.js
@@ -20,7 +20,7 @@ const routes = [
     path: "/ticket",
     name: "Ticket",
     component: () =>
-      import(/* webpackChunkName: "Ticket" */ "../views/Ticket1.vue")
+      import(/* webpackChunkName: "Ticket" */ "../views/Ticket.vue")
   },
   {
     path: "*",
